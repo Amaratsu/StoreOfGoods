c0f8c5f2561c721e1e60a5f2c49dc9967b35ea06
Alexander <imperaxel@gmail.com>
2017-03-08 23:52:26 +0300

    Two projects have been added: the domain stores the entities and the web UI. The product entity, the connection string, for interaction with the database, the context of the interaction of the framework entities, as well as the initialization of products by the code first method.

---
+++ Domain/Entities/ProductDbInitializer.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class ProductDbInitializer : DropCreateDatabaseAlways<EfDbContext>
+    {
+        protected override void Seed(EfDbContext db)
+        {
+            db.Products.Add(new Product
+            {
+                Name = "Xiaomi Redmi 4 16GB Gray",
+                Description =
+                    "Android, экран 5 IPS(720x1280), ОЗУ 2 ГБ, флэш - память 16 ГБ, карты памяти, камера 13 Мп, аккумулятор 4100 мАч, 2 SIM, цвет темно - серый",
+                Category = "Смартфон",
+                Price = 319.00m
+            });
+            db.Products.Add(new Product
+            {
+                Name = "Apple iPhone 7 32GB Black",
+                Description =
+                    "Apple iOS, экран 4.7\" IPS (750x1334), ОЗУ 2 ГБ, флэш-память 32 ГБ, камера 12 Мп, аккумулятор 1960 мАч, 1 SIM, цвет черный",
+                Category = "Смартфон",
+                Price = 1600.00m
+            });
+            db.Products.Add(new Product
+            {
+                Name = "Samsung Galaxy S7 Edge 32GB Black Onyx [G935F]",
+                Description =
+                    "Android, экран 5.5\" AMOLED (1440x2560), ОЗУ 4 ГБ, флэш-память 32 ГБ, карты памяти, камера 12 Мп, аккумулятор 3600 мАч, 1 SIM, цвет черный",
+                Category = "Смартфон",
+                Price = 1350.00m
+            });
+            db.Products.Add(new Product
+            {
+                Name = "iPhone 5s 16GB Space Gray",
+                Description =
+                    "Apple iOS, экран 4\" IPS (640x1136), ОЗУ 1 ГБ, флэш-память 16 ГБ, камера 8 Мп, аккумулятор 1560 мАч, 1 SIM, цвет темно-серый",
+                Category = "Смартфон",
+                Price = 680.00m
+            });
+            db.Products.Add(new Product
+            {
+                Name = "A4Tech Bloody V7",
+                Description =
+                   "полноразмерная игровая мышь для ПК, проводная, USB, сенсор оптический 3200 dpi, 8 кнопок, колесо с нажатием, цвет черный",
+                Category = "Игровая мышь",
+                Price = 36.44m
+            });
+            db.Products.Add(new Product
+            {
+                Name = "A4Tech Bloody V8",
+                Description =
+                   "полноразмерная игровая мышь для ПК, проводная, USB, сенсор оптический 3200 dpi, 8 кнопок, колесо с нажатием, цвет черный",
+                Category = "Игровая мышь",
+                Price = 36.66m
+            });
+            db.Products.Add(new Product
+            {
+                Name = "Logitech G403 Prodigy [910-004824]",
+                Description =
+                   "пполноразмерная игровая мышь для ПК, проводная, USB, сенсор оптический 12000 dpi, 6 кнопок, колесо с нажатием, цвет черный",
+                Category = "Игровая мышь",
+                Price = 124.30m
+            });
+
+            base.Seed(db);
+        }
+    }
+}
---
+++ WebUI/Controllers/ProductController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Domain.Entities;
+using WebUI.Models;
+
+namespace WebUI.Controllers
+{
+    public class ProductController : Controller
+    {
+        private EfDbContext db = new EfDbContext();
+        public int PageSize = 4;
+
+        public ViewResult List(int page = 1)
+        {
+            ProductsListViewModel model = new ProductsListViewModel
+            {
+                Products = db.Products
+                .OrderBy(p => p.ProductId)
+                .Skip((page - 1) * PageSize)
+                .Take(PageSize),
+                PagingInfo = new PagingInfo
+                {
+                    CurrentPage = page,
+                    ItemsPerPage = PageSize,
+                    TotalItems = db.Products.Count()
+                }
+            };
+            return View(model);
+        }
+    }
+}
